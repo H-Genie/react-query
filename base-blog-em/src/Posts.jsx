43baabe01d7c0c0c16b42795874e7412b014aa1e
h-genie <ses2000hj@hanmail.net>
2022-12-26 22:49:42 +0900

    section 2 complete

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,46 +1,62 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useQuery, useQueryClient } from "react-query";
 
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
-async function fetchPosts() {
-  const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
-  );
-  return response.json();
+async function fetchPosts(pageNum) {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`);
+    return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
-  const [selectedPost, setSelectedPost] = useState(null);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const queryClient = useQueryClient();
 
-  return (
-    <>
-      <ul>
-        {data.map((post) => (
-          <li
-            key={post.id}
-            className="post-title"
-            onClick={() => setSelectedPost(post)}
-          >
-            {post.title}
-          </li>
-        ))}
-      </ul>
-      <div className="pages">
-        <button disabled onClick={() => {}}>
-          Previous page
-        </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
-          Next page
-        </button>
-      </div>
-      <hr />
-      {selectedPost && <PostDetail post={selectedPost} />}
-    </>
-  );
+    useEffect(() => {
+        if (currentPage < maxPostPage) {
+            const nextPage = currentPage + 1;
+            queryClient.prefetchQuery(["posts", nextPage], () => fetchPosts(nextPage));
+        }
+    }, [currentPage, queryClient]);
+
+    // replace with useQuery
+    const { data, isError, error, isLoading } = useQuery(["posts", currentPage], () => fetchPosts(currentPage), {
+        staleTime: 2000,
+        keepPreviousData: true,
+    });
+    if (isLoading) return <h3>Loading...</h3>;
+    if (isError)
+        return (
+            <>
+                <h3>Oops, something went worng</h3>
+                <p>{error.toString()}</p>
+            </>
+        );
+
+    return (
+        <>
+            <ul>
+                {data.map((post) => (
+                    <li key={post.id} className="post-title" onClick={() => setSelectedPost(post)}>
+                        {post.title}
+                    </li>
+                ))}
+            </ul>
+            <div className="pages">
+                <button disabled={currentPage <= 1} onClick={() => setCurrentPage((previousValue) => previousValue - 1)}>
+                    Previous page
+                </button>
+                <span>Page {currentPage}</span>
+                <button disabled={currentPage >= maxPostPage} onClick={() => setCurrentPage((previousValue) => previousValue + 1)}>
+                    Next page
+                </button>
+            </div>
+            <hr />
+            {selectedPost && <PostDetail post={selectedPost} />}
+        </>
+    );
 }
